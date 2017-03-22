97a44d38da5257747fca202255061f41a55e5889
MiLeung <michaeleleung@gmail.com>
2017-03-21 23:57:07 -0700

    works for ios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+console.disableYellowBox = true;
+
 import React from 'react';
 import Camera from 'react-native-camera';
+import Video from 'react-native-video';
 import {
   View,
   StyleSheet,
@@ -13,29 +16,53 @@ import TouchableElastic from 'touchable-elastic';
 export default class App extends React.Component {
   state = {
     recording: false,
-    previewPath: '',
+    videoPath: '',
     cameraType: Camera.constants.Type.back,
     recordButtonColor: new Animated.Value(0)
   }
 
   render() {
-    let { recording, cameraType, recordButtonColor } = this.state;
+    let { videoPath, recording, cameraType, recordButtonColor } = this.state;
+    console.log('VIDEOPATH', videoPath)
+
+    let preview = videoPath ? (
+      <Video
+        source={{ uri: videoPath }}
+        repeat={true}
+        volume={1.0}
+        muted={false}
+        style={styles.preview}
+      />
+    ) : (
+      <Camera
+        ref={ref => this.cam = ref}
+        captureMode={Camera.constants.CaptureMode.video}
+        captureTarget={Camera.constants.CaptureTarget.disk}
+        style={styles.preview}
+        captureAudio={true}
+        type={cameraType}
+      />
+    );
+
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
-        <Camera
-          style={styles.camera}
-          type={cameraType}
-          >
-          <TouchableElastic
-            style={styles.cameraIconContainer}
-            onPress={this.toggleCamera}
-            >
-            <Image
-              style={styles.cameraIcon}
-              source={require('./camera.png')}
-            />
-          </TouchableElastic>
+        {preview}
+        <View style={styles.controlPanel}>
+          <View style={styles.topButtonsContainer}>
+            <TouchableElastic onPress={this.removeVideo}>
+              <Image
+                style={[styles.xIcon, { opacity: videoPath ? 1 : 0 }]}
+                source={require('./x.png')}
+              />
+            </TouchableElastic>
+            <TouchableElastic onPress={this.toggleCamera}>
+              <Image
+                style={styles.cameraIcon}
+                source={require('./camera.png')}
+              />
+            </TouchableElastic>
+          </View>
           <TouchableElastic
             style={styles.recordButtonContainer}
             onPressIn={this.startRecording}
@@ -43,24 +70,48 @@ export default class App extends React.Component {
             >
             <Animated.View style={[styles.recordButton, { backgroundColor: recordButtonColor.interpolate({ inputRange: [0, 1], outputRange: ['#fff', '#ff3333'] }) }]} />
           </TouchableElastic>
-        </Camera>
+        </View>
       </View>
     );
   }
 
   toggleCamera = () => {
-    let type = this.state.cameraType;
-    this.setState({ cameraType: type === Camera.constants.Type.back ? Camera.constants.Type.front : Camera.constants.Type.back });
+    let { videoPath, cameraType } = this.state;
+    let { front, back } = Camera.constants.Type;
+    if (videoPath) {
+      return null;
+    }
+    let type = cameraType;
+    this.setState({ cameraType: type === back ? front : back });
   }
 
   startRecording = () => {
+    if (this.state.videoPath) {
+      return null;
+    }
     this.setState({ recording: true });
     this.toggleButtonColor(1);
+    this.cam.capture()
+      .then(data => {
+        this.setState({ videoPath: data.path });
+      })
   }
 
   stopRecording = () => {
+    if (this.state.videoPath) {
+      return null;
+    }
     this.setState({ recording: false });
     this.toggleButtonColor(0);
+    this.cam.stopCapture();
+  }
+
+  removeVideo = () => {
+    let { videoPath } = this.state;
+    if (!videoPath) {
+      return nul;
+    }
+    this.setState({ videoPath: '' });
   }
 
   toggleButtonColor = toValue => {
@@ -76,14 +127,24 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#000',
   },
-  camera: {
-    flex: 1,
+  preview: {
+    flex: 1
+  },
+  controlPanel: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
     justifyContent: 'space-between'
   },
-  cameraIconContainer: {
-    marginTop: 25,
-    marginRight: 10,
-    alignSelf: 'flex-end'
+  topButtonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 10,
+    marginTop: 25
+  },
+  xIcon: {
+    height: 25,
+    width: 25
   },
   cameraIcon: {
     height: 35,
