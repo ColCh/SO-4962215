a9cab95ab9782f11c4ec2a9d1de42fc58cf290a7
Max Sysoev <colchgh@gmail.com>
2017-07-16 10:56:50 +0300

    Initial commit for answer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   StyleSheet,
   StatusBar,
   Animated,
+  TouchableHighlight,
   Image
 } from 'react-native';
 
@@ -20,105 +21,51 @@ export default class App extends React.Component {
     cameraType: Camera.constants.Type.back,
     recordButtonColor: new Animated.Value(0)
   }
+  
+  takeVideo() {
+   console.log('started to take video');
+   this.camera.capture({
+     audio: true,
+     mode: Camera.constants.CaptureMode.still,
+     target: Camera.constants.CaptureTarget.disk
+   }).then((data) => {
+     this.setState({ path: data.path });
+     console.log(data);
+   }).catch((err) => console.log(err));
+ }
+
+ stopVideo() {
+   this.camera.stopCapture();
+   console.log(this.state.path);
+ }
+
+ renderCamera() {
+   return (
+     <View style={{ flex: 1 }}>
+       <Camera
+         ref={(cam) => {
+           this.camera = cam;
+         }}
+         style={styles.preview}
+         aspect={Camera.constants.Aspect.fill}
+         captureTarget={Camera.constants.CaptureTarget.disk}
+         captureMode={Camera.constants.CaptureMode.still}
+       >
+         <TouchableHighlight
+           style={styles.capture}
+           onPressIn={this.takeVideo.bind(this)}
+           onPressOut={this.stopVideo.bind(this)}
+           underlayColor="rgba(255, 255, 255, 0.5)"
+         >
+           <View />
+         </TouchableHighlight>
+       </Camera>
+     </View>
+   );
+ }
 
   render() {
-    let { videoPath, recording, cameraType, recordButtonColor } = this.state;
-    console.log('VIDEOPATH', videoPath)
-
-    let preview = videoPath ? (
-      <Video
-        source={{ uri: videoPath }}
-        repeat={true}
-        volume={1.0}
-        muted={false}
-        style={styles.preview}
-      />
-    ) : (
-      <Camera
-        ref={ref => this.cam = ref}
-        captureMode={Camera.constants.CaptureMode.video}
-        captureTarget={Camera.constants.CaptureTarget.disk}
-        style={styles.preview}
-        captureAudio={true}
-        type={cameraType}
-      />
-    );
-
-    return (
-      <View style={styles.container}>
-        <StatusBar barStyle="light-content" />
-        {preview}
-        <View style={styles.controlPanel}>
-          <View style={styles.topButtonsContainer}>
-            <TouchableElastic onPress={this.removeVideo}>
-              <Image
-                style={[styles.xIcon, { opacity: videoPath ? 1 : 0 }]}
-                source={require('./x.png')}
-              />
-            </TouchableElastic>
-            <TouchableElastic onPress={this.toggleCamera}>
-              <Image
-                style={styles.cameraIcon}
-                source={require('./camera.png')}
-              />
-            </TouchableElastic>
-          </View>
-          <TouchableElastic
-            style={styles.recordButtonContainer}
-            onPressIn={this.startRecording}
-            onPressOut={this.stopRecording}
-            >
-            <Animated.View style={[styles.recordButton, { backgroundColor: recordButtonColor.interpolate({ inputRange: [0, 1], outputRange: ['#fff', '#ff3333'] }) }]} />
-          </TouchableElastic>
-        </View>
-      </View>
-    );
-  }
-
-  toggleCamera = () => {
-    let { videoPath, cameraType } = this.state;
-    let { front, back } = Camera.constants.Type;
-    if (videoPath) {
-      return null;
-    }
-    let type = cameraType;
-    this.setState({ cameraType: type === back ? front : back });
-  }
-
-  startRecording = () => {
-    if (this.state.videoPath) {
-      return null;
-    }
-    this.setState({ recording: true });
-    this.toggleButtonColor(1);
-    this.cam.capture()
-      .then(data => {
-        this.setState({ videoPath: data.path });
-      })
-  }
-
-  stopRecording = () => {
-    if (this.state.videoPath) {
-      return null;
-    }
-    this.setState({ recording: false });
-    this.toggleButtonColor(0);
-    this.cam.stopCapture();
-  }
-
-  removeVideo = () => {
-    let { videoPath } = this.state;
-    if (!videoPath) {
-      return nul;
-    }
-    this.setState({ videoPath: '' });
-  }
-
-  toggleButtonColor = toValue => {
-    Animated.timing(this.state.recordButtonColor, {
-      toValue,
-      duration: 100
-    }).start();
+    return this.renderCamera();
   }
 }
 
